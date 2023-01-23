cafb2be42a869c708198fd290dd8de2c9652a97f
paulgilchrist <PGilchrist@pulte.com>
2023-01-23 13:42:11 -0500

    Package updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,7 +36,7 @@ const routes: Routes = [
  ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: NoPreloading, relativeLinkResolution: 'legacy' })
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: NoPreloading })
 ],
   exports: [RouterModule]
 })
