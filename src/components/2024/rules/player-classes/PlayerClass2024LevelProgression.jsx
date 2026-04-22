512b602d41f63c89e375f3fc9533f3bb7f25142c
paulgilchrist <PGilchrist@pulte.com>
2026-04-22 19:43:34 -0400

    Add spellcasting info display to 2024 class level progression

--- src/components/2024/rules/player-classes/PlayerClass2024LevelProgression.jsx
+++ src/components/2024/rules/player-classes/PlayerClass2024LevelProgression.jsx
@@ -38,43 +38,11 @@ function PlayerClass2024LevelProgression({ playerClass, shownLevel, onShowLevel 
 }
 
 /**
- * Component to display all levels and their features
- */
-function AllLevelsView({ playerClass }) {
-    return (
-        <div>
-            {playerClass.class_levels.map((level) => (
-                <div key={level.level} id={level.level} className="class2024-level-view">
-                    <h5 className="level-header">
-                        Level {level.level} <span className="proficiency-badge">Proficiency: {level.proficiency_bonus}</span>
-                    </h5>
-                    {level.features && level.features.length > 0 && (
-                        <div className="level-features">
-                            {level.features.map((feature, fIndex) => (
-                                <div key={fIndex} className="feature-item class2024-feature-item">
-                                    <b>{feature.name}:</b>&nbsp;
-                                    {feature.type === 'subclass_feature' && (
-                                        <span className="subclass-badge">(Subclass)</span>
-                                    )}
-                                    {feature.description && (
-                                        <span dangerouslySetInnerHTML={{ __html: feature.description }} />
-                                    )}
-                                </div>
-                            ))}
-                        </div>
-                    )}
-                </div>
-            ))}
-        </div>
-    );
-}
-
-/**
  * Component to display features for a selected level
  */
 function SelectedLevelView({ playerClass, shownLevel }) {
     const selectedLevel = playerClass.class_levels.find(l => l.level === shownLevel);
-    
+
     if (!selectedLevel) {
         return null;
     }
@@ -97,9 +65,9 @@ function SelectedLevelView({ playerClass, shownLevel }) {
 
     return (
         <div className="selected-level-features">
-            <h5 className="level-header class2024-level-header">
-                Level {selectedLevel.level} <span className="proficiency-badge">Proficiency: {selectedLevel.proficiency_bonus}</span>
-            </h5>
+            <b>Proficiency:</b> {selectedLevel.proficiency_bonus}
+            {/* Spellcasting Info */}
+            {selectedLevel.spellcasting && renderSpellcastingInfo(selectedLevel.spellcasting)}<br/>
             {allFeatures.map((feature, fIndex) => (
                 <div key={fIndex} className="feature-item class2024-feature-item">
                     <b>Level {feature.sourceLevel}: {feature.name}:</b>&nbsp;
@@ -111,6 +79,65 @@ function SelectedLevelView({ playerClass, shownLevel }) {
                     )}
                 </div>
             ))}
+        </div>
+    );
+}
+
+/**
+ * Helper function to render spellcasting information
+ * Uses existing 5E styling classes for consistency
+ */
+function renderSpellcastingInfo(spellcasting) {
+    const slots = [];
+
+    if (spellcasting.spell_slots_level_1 > 0) {
+        slots.push(<div key="level1">&nbsp;&nbsp;&nbsp;&nbsp;{spellcasting.spell_slots_level_1} of level 1</div>);
+    }
+    if (spellcasting.spell_slots_level_2 > 0) {
+        slots.push(<div key="level2">&nbsp;&nbsp;&nbsp;&nbsp;{spellcasting.spell_slots_level_2} of level 2</div>);
+    }
+    if (spellcasting.spell_slots_level_3 > 0) {
+        slots.push(<div key="level3">&nbsp;&nbsp;&nbsp;&nbsp;{spellcasting.spell_slots_level_3} of level 3</div>);
+    }
+    if (spellcasting.spell_slots_level_4 > 0) {
+        slots.push(<div key="level4">&nbsp;&nbsp;&nbsp;&nbsp;{spellcasting.spell_slots_level_4} of level 4</div>);
+    }
+    if (spellcasting.spell_slots_level_5 > 0) {
+        slots.push(<div key="level5">&nbsp;&nbsp;&nbsp;&nbsp;{spellcasting.spell_slots_level_5} of level 5</div>);
+    }
+    if (spellcasting.spell_slots_level_6 > 0) {
+        slots.push(<div key="level6">&nbsp;&nbsp;&nbsp;&nbsp;{spellcasting.spell_slots_level_6} of level 6</div>);
+    }
+    if (spellcasting.spell_slots_level_7 > 0) {
+        slots.push(<div key="level7">&nbsp;&nbsp;&nbsp;&nbsp;{spellcasting.spell_slots_level_7} of level 7</div>);
+    }
+    if (spellcasting.spell_slots_level_8 > 0) {
+        slots.push(<div key="level8">&nbsp;&nbsp;&nbsp;&nbsp;{spellcasting.spell_slots_level_8} of level 8</div>);
+    }
+    if (spellcasting.spell_slots_level_9 > 0) {
+        slots.push(<div key="level9">&nbsp;&nbsp;&nbsp;&nbsp;{spellcasting.spell_slots_level_9} of level 9</div>);
+    }
+
+    return (
+        <div className="playerClass-margin-bottom-small">
+            {spellcasting.cantrips_known > 0 && (
+                <div>
+                    <b>Cantrips Known:</b>&nbsp;{spellcasting.cantrips_known}<br />
+                </div>
+            )}
+
+            {spellcasting.spells_known > 0 && (
+                <div>
+                    <b>Spells Known:</b>&nbsp;{spellcasting.spells_known}<br />
+                </div>
+            )}
+
+            {slots.length > 0 && (
+                <div>
+                    <b>Spell Slots:</b><br />
+                    {slots}
+                </div>
+            )}
         </div>
     );
 }
