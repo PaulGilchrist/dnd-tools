6e60ad37d0a02e397c2f65cfe9d0c8993ee278fc
paulgilchrist <PGilchrist@pulte.com>
2026-05-02 12:50:41 -0400

    Fix vitest exclusions to remove jpg and css

--- vitest.config.js
+++ vitest.config.js
@@ -2,26 +2,43 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-   plugins: [react()],
-   test: {
-     globals: true,
-     environment: 'jsdom',
-     setupFiles: './src/test/setup.js',
-     environmentOptions: {
-        jsdom: {
-          url: 'http://localhost/',
-         },
-       },
-      define: {
-          'globalThis': 'globalThis',
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/test/setup.js',
+        environmentOptions: {
+            jsdom: {
+                url: 'http://localhost/',
+            },
         },
-    coverage: {
-      provider: 'v8',
-       reportsDirectory: './coverage',
-        exclude: [
-             'node_modules/',
-              'src/test/',
-           ],
-          },
-         },
-      });
+        define: {
+            'globalThis': 'globalThis',
+        },
+        coverage: {
+            provider: 'v8',
+            reportsDirectory: './coverage',
+            exclude: [
+                'node_modules/',
+                'src/test/',
+                '**/*.css',
+                '**/*.scss',
+                '**/*.sass',
+                '**/*.less',
+                '**/*.jpg',
+                '**/*.jpeg',
+                '**/*.png',
+                '**/*.gif',
+                '**/*.svg',
+                '**/*.webp',
+                '**/*.ico',
+                '**/*.mp3',
+                '**/*.mp4',
+                '**/*.woff',
+                '**/*.woff2',
+                '**/*.ttf',
+                '**/*.eot'
+            ],
+        },
+    },
+});
